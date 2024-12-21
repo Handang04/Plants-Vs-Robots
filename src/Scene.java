b8088d0d4be2bf4ffe3673dadecf324768ebb639
Handang04 <maihandang.work@gmail.com>
2024-12-21 18:18:51 +0700

    added interface in Scene class

--- src/Scene.java
+++ src/Scene.java
@@ -1,8 +1,19 @@
 import acm.graphics.*;
+
+import java.awt.event.MouseEvent;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Scene extends GraphicsApplication{
+interface  Interfaceable{
+	public void showContents();
+	public void hideContents();
+	public void mousePressed(MouseEvent e);
+	public void mouseReleased(MouseEvent e);
+	public void mouseClicked(MouseEvent e);
+	public void mouseDragged(MouseEvent e);
+}
+
+public class Scene extends GraphicsApplication implements Interfaceable{
 	/*
 	 * To ensure there are no duplicate graphical objects, create a set of activeContents
 	 * A HashSet is a specific implementation of Set interface, providng constant-time performance for basic operations
@@ -20,6 +31,37 @@ public class Scene extends GraphicsApplication{
 			activeContents.remove(element);
 		}
 		return;
+	}
+
+	@Override
+	public void showContents() {
+		System.out.println("Show contents!");
+		
+	}
+
+	@Override
+	public void hideContents() {
+		System.out.println("Hide contents!");
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		System.out.println("MOUSE PRESSED");
+	}
+	
+	@Override 
+	public void mouseReleased(MouseEvent e) {
+		System.out.println("MOUSE RELEASED");
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("MOUSE CLICKED");
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		System.out.println("MOUSE DRAGGED");
 	}
 
 }
